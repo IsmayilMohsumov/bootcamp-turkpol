e1b0bee3824d6983a24c02cb6271fd02940f0f19
Ismayil <mohsumovismayil@gmail.com>
2024-06-01 14:16:16 +0200

    first commit

---
+++ src/main/java/turkpol/org/pl/bootcamp_turkpol/BootcampTurkpolApplication.java
@@ -0,0 +1,13 @@
+package turkpol.org.pl.bootcamp_turkpol;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootcampTurkpolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootcampTurkpolApplication.class, args);
+	}
+
+}
---
+++ src/test/java/turkpol/org/pl/bootcamp_turkpol/BootcampTurkpolApplicationTests.java
@@ -0,0 +1,13 @@
+package turkpol.org.pl.bootcamp_turkpol;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BootcampTurkpolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
